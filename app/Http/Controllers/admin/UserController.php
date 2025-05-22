4aebb74e5965b4d8a2f24d3bc9d7169d0d6d9174
sirajulgit <sirajulali008@gmail.com>
2025-05-22 18:33:16 +0530

    user changes 7

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
     public function create()
     {
 
-        $items = User::orderBy("order_no", "desc")->first();
+        $items = User::where('user_type', 2)->orderBy("order_no", "desc")->first();
         // dd($items);
 
         $data = array();
