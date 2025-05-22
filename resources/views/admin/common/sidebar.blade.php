cbc591f9b21b799e296a23098ac55aa17616fedb
sirajulgit <sirajulali008@gmail.com>
2025-05-22 14:42:40 +0530

    admin changes1

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('dashboard')}}" class="brand-link">
-        <img src="{{ asset('asset/dist/img/circle_admin_logo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+
         <span class="brand-text font-weight-light">Admin</span>
     </a>
 
@@ -126,7 +125,7 @@
                     </ul>
                 </li>
 
-                
+
 
 
 
