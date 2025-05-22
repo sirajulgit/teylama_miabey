d8af9f29b826e7eb6b342a2838dd7d725d4f7aed
sirajulgit <sirajulali008@gmail.com>
2025-05-23 02:45:03 +0530

    currency product 3

--- resources/views/admin/product_create.blade.php
+++ resources/views/admin/product_create.blade.php
@@ -74,8 +74,6 @@
                     amount: {
                         required: true,
                          number: true
-
-
                     }
                 },
                 messages: {
--- resources/views/admin/product_edit.blade.php
+++ resources/views/admin/product_edit.blade.php
@@ -9,47 +9,32 @@
                     <div class="card">
 
                         <div class="card-header">
-                            <h3 class="card-title">User Edit</h3>
+                            <h3 class="card-title">Product Edit</h3>
 
                             <div class="card-tools">
-                                <a href="{{ route('user_list') }}" class="btn btn-primary">Back</a>
+                                <a href="{{ route('product_list') }}" class="btn btn-primary">Back</a>
                             </div>
                         </div>
                         <!-- /.card-header -->
 
-                        <form id="quickForm" action="{{ route('post_user_edit', $data['item']['id']) }}" method="POST"
+                        <form id="quickForm" action="{{ route('post_product_edit', $data['item']['id']) }}" method="POST"
                             enctype="multipart/form-data">
                             @csrf
 
-                             <div class="card-body row">
+                            <div class="card-body row">
 
                                 <div class="form-group col-md-6">
-                                    <label for="name">Name</label>
-                                    <input type="text" name="name" class="form-control" id="name"
-                                          value="{{ $data['item']['name'] }}">
+                                    <label for="widthdraw_perc">Widthdraw %</label>
+                                    <input type="text" name="widthdraw_perc" class="form-control" id="widthdraw_perc"
+                                        value="{{ $data['item']['widthdraw_perc'] }}">
                                 </div>
 
                                 <div class="form-group col-md-6">
-                                    <label for="email">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email"
-                                        value="{{ $data['item']['email'] }}">
-                                </div>
-                                  <div class="form-group col-md-6">
-                                    <label for="username">Username</label>
-                                    <input type="text" name="username" class="form-control" id="username"
-                                        value="{{ $data['item']['username'] }}">
+                                    <label for="amount">Amount</label>
+                                    <input type="amount" name="amount" class="form-control" id="amount"
+                                       value="{{ $data['item']['amount'] }}">
                                 </div>
 
-<div class="form-group col-md-6">
-                                    <label>Status</label>
-                                    <select class="form-control select2" style="width: 100%;" name="status">
-                                        <option value="1" {{ $data['item']['status'] == 1 ? 'selected' : '' }}>Active
-                                        </option>
-                                        <option value="0" {{ $data['item']['status'] == 0 ? 'selected' : '' }}>No
-                                            Active
-                                        </option>
-                                    </select>
-                                </div>
 
 
 
@@ -82,23 +67,17 @@
             ////////////// form validation ////////////////////////
             $('#quickForm').validate({
                 rules: {
-                    name: {
+                    widthdraw_perc: {
                         required: true,
+                         number: true
                     },
-                    email: {
+                    amount: {
                         required: true,
-                        email:true
-                    },
-                    username: {
-                        required: true,
-
+                         number: true
                     }
                 },
                 messages: {
-                    email: {
-                        required: "Please enter a email address",
-                        email: "Please enter a valid email address"
-                    }
+
                 },
                 errorElement: 'span',
                 errorPlacement: function(error, element) {
@@ -114,34 +93,7 @@
                 submitHandler: function(form, event) {
                     event.preventDefault();
                      form.submit();
-                    // Toast.create("Voila!", "How easy was that?", TOAST_STATUS.SUCCESS, 5000);
 
-                    // var formData = new FormData();
-
-                    // var other_data = $('#chatoutForm').serializeArray();
-                    // $.each(other_data, function(key, input) {
-                    //     formData.append(input.name, input.value);
-                    // });
-
-
-                    // let url = "/cashout";
-
-                    // $.ajax({
-                    //     // headers: {
-                    //     //     'X-CSRF-TOKEN': "{{ csrf_token() }}"
-                    //     // },
-                    //     url: "/cashout",
-                    //     method: "POST",
-                    //     contentType: false,
-                    //     // processData: false,
-                    //     data: formData,
-                    //     success: function(response) {
-                    //         console.log(response);
-                    //     },
-                    //     error: function(error) {
-                    //         console.log("error" + error);
-                    //     }
-                    // });
                 }
             });
             ////////////// end form validation ////////////////////
