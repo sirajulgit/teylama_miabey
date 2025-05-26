fac7913d918d54fa6dea41422fc81e39e24dc7b9
sirajulgit <sirajulali008@gmail.com>
2025-05-26 20:22:10 +0530

    eco

---
+++ resources/views/user/payment_qr.blade.php
@@ -0,0 +1,21 @@
+@extends('user.layout.auth_layout')
+
+@section('content')
+    <header>
+        <div class="top-cover d-flex align-items-center justify-content-between">
+            <button class="btn-back-frw"> <i class="bi bi-chevron-left"></i> </button>
+            <strong class="center-heading">Payment QR Code</strong>
+            <strong> </strong>
+        </div>
+    </header>
+
+    <main class="home-content">
+        <div class="container-fluid">
+            <div class="price-part pt-3 pb-3">
+                <img src="{{ $data['payment_qr_code_img'] }}" alt="QR Code" class="img-fluid mb-3">
+                <p>Payment QR Code: <span style="word-break: break-word;"> {{ $data['payment_qr_code'] }} </span> </p>
+            </div>
+        </div>
+    </main>
+
+@endsection
