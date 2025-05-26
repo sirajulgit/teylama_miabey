ed9508e4449161914c027a54821524e090387e43
sirajulgit <sirajulali008@gmail.com>
2025-05-26 15:42:01 +0530

    buy product 3

--- app/Http/Controllers/user/BuyProductController.php
+++ app/Http/Controllers/user/BuyProductController.php
@@ -14,7 +14,7 @@ class BuyProductController extends Controller
 
     public function index(Request $request)
     {
-        echo $request->id;exit;
+        //echo $request->id;exit;
         $data = [
             'page_title' => 'Buy USDT',
         ];
@@ -22,9 +22,14 @@ class BuyProductController extends Controller
          $currencyitems = CurrencyRate::where("currency","USDT")->get();
        //echo $currencyitems[0]['currency_value'];exit;
         $currency_value = $currencyitems[0]['currency_value'];
-        $productdata = Product::orderBy("id", "asc")->get();
+        $productdata =Product::find($request->id);
+        $data['productdata'] = $productdata;
+        $data['currency_value'] = $currency_value;
+        $data['product_id'] = $request->id;
+        $data['product_amount'] = $productdata->amount;
+        $data['product_widthdraw_perc'] = $productdata->widthdraw_perc;
+        $data['product_currency'] = $productdata->currency;
 
-
-        return view('user.product_buy', []);
+        return view('user.product_buy', $data);
     }
 }
