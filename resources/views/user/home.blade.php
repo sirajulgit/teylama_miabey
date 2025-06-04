9af0e49bb4938a0c27c87d2dee17bc7516830956
sirajulgit <sirajulali008@gmail.com>
2025-06-04 18:34:48 +0530

    home5

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,19 +2,14 @@
 
 
 @section('content')
-
-
-
-
-    <main class="home-content">
-        <div class="container-fluid">
-          Home
-
-
-
-
-        </div>
-    </main>
+<div class="banner-area position-relative">
+  <img src="{{ asset('asset/frontend/images/banner.png')}}">
+  <div class="home-banner-content">
+    <img src="{{ asset('asset/frontend/images/hello-i-am.png')}}">
+    <h1> Teylama Miabey  </h1>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum, metus nec lobortis gravida, sapien eros scelerisque quam. </p>
+  </div>
+ </div>
 
 @endsection
 
