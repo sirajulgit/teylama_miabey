e8dbd066a651a37ef21ed894777c06017b76c108
sirajulgit <sirajulali008@gmail.com>
2025-06-05 17:03:59 +0530

    setting2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,23 +19,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
         //
-        View::composer('user.common.header', function ($view) {
-            $email_data = Cms::where('type', 'contact_email')->get();
-            $gmail_link_data = Cms::where('type', 'gmail_link')->get();
-            $twitter_link_data = Cms::where('type', 'twitter_link')->get();
-            $fb_link_data = Cms::where('type', 'fb_link')->get();
-            $insra_link_data = Cms::where('type', 'insra_link')->get();
-            $youtube_link_data = Cms::where('type', 'youtube_link')->get();
-            $setting_data=array(
-                "contact_email"=>$email_data->details,
-                "gmail_link"=>$gmail_link_data->details,
-                "twitter_link"=>$twitter_link_data->details,
-                "fb_link"=>$fb_link_data->details,
-                "insra_link"=>$insra_link_data->details,
-                "youtube_link"=>$youtube_link_data->details
-            )
-        $view->with('settings', $setting_data);
-    });
+       View::composer('user.common.header', function ($view) {
+    $email_data = Cms::where('type', 'contact_email')->first();
+    $gmail_link_data = Cms::where('type', 'gmail_link')->first();
+    $twitter_link_data = Cms::where('type', 'twitter_link')->first();
+    $fb_link_data = Cms::where('type', 'fb_link')->first();
+    $insra_link_data = Cms::where('type', 'insra_link')->first();
+    $youtube_link_data = Cms::where('type', 'youtube_link')->first();
+
+    $setting_data = [
+        "contact_email" => optional($email_data)->details,
+        "gmail_link"    => optional($gmail_link_data)->details,
+        "twitter_link"  => optional($twitter_link_data)->details,
+        "fb_link"       => optional($fb_link_data)->details,
+        "insra_link"    => optional($insra_link_data)->details,
+        "youtube_link"  => optional($youtube_link_data)->details,
+    ];
+
+    $view->with('settings', $setting_data);
+});
     }
 }
