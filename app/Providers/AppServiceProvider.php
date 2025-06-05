f69864c578a6f033b8f5d10b1b1f866291ef92a9
sirajulgit <sirajulali008@gmail.com>
2025-06-05 16:47:40 +0530

    setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\Cms;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +20,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        View::composer('user.common.header', function ($view) {
+            $email_data = Cms::where('type', 'contact_email')->get();
+            $gmail_link_data = Cms::where('type', 'gmail_link')->get();
+            $twitter_link_data = Cms::where('type', 'twitter_link')->get();
+            $fb_link_data = Cms::where('type', 'fb_link')->get();
+            $insra_link_data = Cms::where('type', 'insra_link')->get();
+            $youtube_link_data = Cms::where('type', 'youtube_link')->get();
+            $setting_data=array(
+                "contact_email"=>$email_data->details,
+                "gmail_link"=>$gmail_link_data->details,
+                "twitter_link"=>$twitter_link_data->details,
+                "fb_link"=>$fb_link_data->details,
+                "insra_link"=>$insra_link_data->details,
+                "youtube_link"=>$youtube_link_data->details
+            )
+        $view->with('settings', $setting_data);
+    });
     }
 }
