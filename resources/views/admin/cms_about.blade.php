a0857e7f83d69f05ec48e4fecc470dc0bb3ad772
sirajulgit <sirajulali008@gmail.com>
2025-06-06 12:08:40 +0530

    admin change

--- resources/views/admin/cms_about.blade.php
+++ resources/views/admin/cms_about.blade.php
@@ -304,7 +304,7 @@
                     <div class="card">
 
                         <div class="card-header">
-                            <h3 class="card-title badge badge-success font-weight-bold">What We Do</h3>
+                            <h3 class="card-title badge badge-success font-weight-bold">Politician</h3>
                         </div>
                         <!-- /.card-header -->
 
