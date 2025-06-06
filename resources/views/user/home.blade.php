7f70c9e38b8366f6931bb7ef9a12ea62148a512e
sirajulgit <sirajulali008@gmail.com>
2025-06-07 02:23:27 +0530

    home18

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -101,7 +101,7 @@
             <div>
   @if (count($homedata['what_we_do']['badge_data']) > 0)
                                             @foreach ($homedata['what_we_do']['badge_data'] as $index => $item)
-              <button class="nav-link text-start  @if ($index == 0) active @endif" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-{{$item['badge_text_1']}}" type="button" role="tab" aria-controls="v-pills-{{$item['badge_text_1']}}" aria-selected="true">0{{$index + 1}}. {{$item['badge_text_1']}}</button>
+              <button class="nav-link text-start  @if ($index == 0) active @endif" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-{{$item['id']}}" type="button" role="tab" aria-controls="v-pills-{{$item['id']}}" aria-selected="true">0{{$index + 1}}. {{$item['badge_text_1']}}</button>
               @endforeach
               @endif
 
@@ -112,7 +112,7 @@
           </div>
         <div class="tab-content" id="v-pills-tabContent">
  @foreach ($homedata['what_we_do']['badge_data'] as $index => $item)
-        <div class="tab-pane fade show @if ($index == 0) active @endif" id="v-pills-{{$item['badge_text_1']}}" role="tabpanel" aria-labelledby="v-pills-{{$item['badge_text_1']}}-tab">
+        <div class="tab-pane fade show @if ($index == 0) active @endif" id="v-pills-{{$item['id']}}" role="tabpanel" aria-labelledby="v-pills-{{$item['id']}}-tab">
           <div class="row">
             <div class="col-lg-6">
               <div class="tab-content-area">
