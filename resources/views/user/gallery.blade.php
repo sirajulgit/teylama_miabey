3bdfdb62a976e4306d71eb244f6169cea1181a8a
sirajulgit <sirajulali008@gmail.com>
2025-06-07 12:56:11 +0530

    chnages

--- resources/views/user/gallery.blade.php
+++ resources/views/user/gallery.blade.php
@@ -34,21 +34,6 @@
             <div class="gallerys tz-gallery">
                 <div id="image" class="tabcontent">
                     <div class="row no-gutters">
-
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-4">
-                            <div class="gallery-holder">
-                                <img src="{{ asset('asset/frontend/images/rashio-gallery.png') }}" alt="empty">
-                                <div class="holder"
-                                    style="background: url({{ asset('asset/frontend/images/gallery-1.png') }});">
-                                    <div class="capson">
-                                        <a class="lightbox" href="{{ asset('asset/frontend/images/gallery-1.png') }}">
-                                            <i class="fa fa-search"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         @foreach ($data['gallery_data'] as $item)
                             <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-4">
                                 <div class="gallery-holder">
@@ -63,9 +48,47 @@
                                 </div>
                             </div>
                         @endforeach
-
                     </div>
                 </div>
+
+
+                {{-- ########### | Pagination | ########### --}}
+                @if ($data['gallery_data']->hasPages())
+                    <nav class="Page navigation pagination-area mt-4">
+                        <ul class="pagination">
+                            {{-- Previous Page Link --}}
+                            @if ($data['gallery_data']->onFirstPage())
+                                <li class="page-item disabled"><span class="page-link">Previous</span></li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $data['gallery_data']->previousPageUrl() }}"
+                                        rel="prev">Previous</a>
+                                </li>
+                            @endif
+
+                            {{-- Pagination Elements --}}
+                            @foreach ($data['gallery_data']->links()->elements[0] as $page => $url)
+                                @if ($page == $data['gallery_data']->currentPage())
+                                    <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+                                @else
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                                    </li>
+                                @endif
+                            @endforeach
+
+                            {{-- Next Page Link --}}
+                            @if ($data['gallery_data']->hasMorePages())
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $data['gallery_data']->nextPageUrl() }}"
+                                        rel="next">Next</a>
+                                </li>
+                            @else
+                                <li class="page-item disabled"><span class="page-link">Next</span></li>
+                            @endif
+                        </ul>
+                    </nav>
+                @endif
             </div>
         </div>
     </div>
