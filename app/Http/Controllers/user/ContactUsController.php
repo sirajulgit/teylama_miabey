602a547fe5f75f63d6899a933722efd103f15a8e
sirajulgit <sirajulali008@gmail.com>
2025-06-07 13:04:32 +0530

    chnages

--- app/Http/Controllers/user/ContactUsController.php
+++ app/Http/Controllers/user/ContactUsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\CmsBanner;
+use App\Models\CmsContact;
 use Illuminate\Http\Request;
 
 
@@ -15,7 +17,26 @@ class ContactUsController extends Controller
         $data = [
             'page_title' => 'Contact Us',
             'activePageName' => 'contact_us',
+            'contact_data' => [],
+            'banner_data' => [],
         ];
+
+        // +++++++++++++++ | CONTACT Data | +++++++++++++++
+        $data['contact_data'] = CmsContact::first();
+
+
+        // +++++++++++++++ | CMS BANNER | +++++++++++++++
+        $cmsBanner = CmsBanner::where('type', 'contact_page')->orderBy("id", "asc")->get()->toArray();
+
+        foreach ($cmsBanner as $item) {
+
+            if (!is_null($item['image'])) {
+                $default_image = '/uploads/images/' . $item['image'];
+                $item['image'] = $default_image;
+            }
+
+            array_push($data['banner_data'], $item);
+        }
 
 
         return view('user.contact_us', ['data' => $data]);
--- resources/views/user/contact_us.blade.php
+++ resources/views/user/contact_us.blade.php
@@ -2,86 +2,85 @@
 
 
 @section('content')
-
-<div class="banner-area position-relative">
-<div class="inner-banner">
-<div>
-<img src="{{ asset('asset/frontend/images/banner.png')}}">
-</div>
-</div>
-<div class="home-banner-content">
-<img data-aos="fade-right" data-aos-duration="2000" src="{{ asset('asset/frontend/images/hello-i-am.png')}}">
-<h1 data-aos="fade-right" data-aos-duration="2000"> Teylama Miabey  </h1>
-</div>
-</div>
-
-
-  <div class="space-section mt-5 pt-4 pb-4 mb-5">
-    <div class="container">
-      <div class="row">
-
-        <div class="col-lg-4">
-          <div class="details-contact">
-            <h3 class="heading-contact mb-4">Contact Us</h3>
-
-              <div class="foot-description d-flex align-items-center mb-3">
-                  <span class="round-icon"> <i class="bi bi-geo-alt"></i> </span>
-                  <div>
-                    <b>Our Location</b>
-                    <p> XYZ Hilton Street, 125 Town <br> United State </p>
-                  </div>
-              </div>
-
-              <div class="foot-description d-flex align-items-center mb-3">
-                  <span class="round-icon"> <i class="bi bi-telephone"></i> </span>
-                  <div>
-                    <p> 009-215-5599 </p>
-                  </div>
-              </div>
-
-              <div class="foot-description d-flex align-items-center mb-3">
-                <span class="round-icon"> <i class="bi bi-envelope"></i> </span>
+    <div class="banner-area position-relative">
+        <div class="inner-banner">
+            @foreach ($data['banner_data'] as $item)
                 <div>
-                  <p> hello@infopoliticia.com </p>
+                    <img src="{{ asset($item['image']) }}">
                 </div>
-              </div>
-
-          </div>
+            @endforeach
         </div>
-
-        <div class="col-lg-8">
-          <div class="contact-form">
-            <div class="mb-4">
-              <h3 class="heading"> Get A Quote</h3>
-            </div>
-            <div class="row">
-              <div class="col-lg-6">
-                <input class="form-control mb-3" type="text" placeholder="Name">
-              </div>
-              <div class="col-lg-6">
-                <input class="form-control mb-3" type="text" placeholder="Email">
-              </div>
-              <div class="col-lg-12">
-                <input class="form-control mb-3" type="text" placeholder="Phone Number">
-              </div>
-              <div class="col-lg-12">
-                <textarea class="form-control mb-3" placeholder="Message" rows="5"></textarea>
-              </div>
-              <div class="col-lg-12">
-                <input class="submit-btn" type="submit" value="Submit">
-              </div>
-            </div>
-          </div>
+        <div class="home-banner-content">
+            @foreach ($data['banner_data'] as $item)
+                <img data-aos="fade-right" data-aos-duration="2000" src="{{ asset('asset/frontend/images/hello-i-am.png') }}">
+                <h1 data-aos="fade-right" data-aos-duration="2000"> {{ $item['title_1'] }} </h1>
+            @endforeach
         </div>
-
-
-
-
-      </div>
     </div>
-  </div>
+
+
+    <div class="space-section mt-5 pt-4 pb-4 mb-5">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4">
+                    <div class="details-contact">
+                        <h3 class="heading-contact mb-4">Contact Us</h3>
+
+                        <div class="foot-description d-flex align-items-center mb-3">
+                            <span class="round-icon"> <i class="bi bi-geo-alt"></i> </span>
+                            <div>
+                                <b>Our Location</b>
+                                {!! $data['contact_data']['address'] !!}
+                            </div>
+                        </div>
+
+                        <div class="foot-description d-flex align-items-center mb-3">
+                            <span class="round-icon"> <i class="bi bi-telephone"></i> </span>
+                            <div>
+                                <p> {{ $data['contact_data']['phone'] }} </p>
+                            </div>
+                        </div>
+
+                        <div class="foot-description d-flex align-items-center mb-3">
+                            <span class="round-icon"> <i class="bi bi-envelope"></i> </span>
+                            <div>
+                                <p> {{ $data['contact_data']['email'] }} </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-8">
+                    <div class="contact-form">
+                        <div class="mb-4">
+                            <h3 class="heading"> Get A Quote</h3>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <input class="form-control mb-3" type="text" placeholder="Name">
+                            </div>
+                            <div class="col-lg-6">
+                                <input class="form-control mb-3" type="text" placeholder="Email">
+                            </div>
+                            <div class="col-lg-12">
+                                <input class="form-control mb-3" type="text" placeholder="Phone Number">
+                            </div>
+                            <div class="col-lg-12">
+                                <textarea class="form-control mb-3" placeholder="Message" rows="5"></textarea>
+                            </div>
+                            <div class="col-lg-12">
+                                <input class="submit-btn" type="submit" value="Submit">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
 
+
+            </div>
+        </div>
+    </div>
 @endsection
-
