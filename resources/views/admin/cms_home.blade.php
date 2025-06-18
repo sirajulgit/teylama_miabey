fcdcb1a38977b2cadde270f01190be209a353d01
sirajulgit <sirajulali008@gmail.com>
2025-06-18 16:14:57 +0530

    about2

--- resources/views/admin/cms_home.blade.php
+++ resources/views/admin/cms_home.blade.php
@@ -103,33 +103,155 @@
                                         <input type="text" class="form-control text-input" name="about_badge_text1"
                                             value="" placeholder="About Badge Text 1">
 
-
-                                    <div class="admin_upload">
                                         <label for="image">About Badge Icon 1</label>
-                                        <label class="admin-upload-wrap">
-                                            <input type="file" class="form-control mt-3 uploadFile" name="image_2"
-                                                id="image_2_about" accept="image/png, image/jpg, image/jpeg, image/webp">
+                                        <div class="admin_upload">
 
-                                            {{-- @if (!is_null($items['about']['image_2']))
+                                            <label class="admin-upload-wrap">
+                                                <input type="file" class="form-control mt-3 uploadFile" name="image_2"
+                                                    id="image_2_about"
+                                                    accept="image/png, image/jpg, image/jpeg, image/webp">
+
+                                                {{-- @if (!is_null($items['about']['image_2']))
                                                 <button type="button" data-id="{{ $items['about']['id'] }}"
                                                     data-field-name="image_2"
                                                     class="btn btn-sm btn-danger del_about_right_img mt-3 mb-3"><i class="fas fa-trash" aria-hidden="true"></i> Delete</button>
                                             @endif --}}
-                                        </label>
+                                            </label>
 
-                                        <div class="profile_image">
+                                            <div class="profile_image">
 
-                                            @if ($items['about']['image_2'])
-                                                <img class="profile_img" id="thumbnail_show_image_2_about"
-                                                    src="{{ $items['about']['image_2'] }}" width="148px" height="221px">
-                                            @else
-                                                <img class="profile_img" id="thumbnail_show_image_2_about"
-                                                    src="{{ asset('asset/images/default_image.png') }}" width="148px"
-                                                    height="221px">
-                                            @endif
+                                                @if ($items['about']['image_2'])
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ $items['about']['image_2'] }}" width="148px"
+                                                        height="221px">
+                                                @else
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ asset('asset/images/default_image.png') }}"
+                                                        width="148px" height="221px">
+                                                @endif
+                                            </div>
                                         </div>
                                     </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="image">About Badge Text 1</label>
+                                        <input type="text" class="form-control text-input" name="about_badge_text1"
+                                            value="" placeholder="About Badge Text 1">
+
+                                        <label for="image">About Badge Icon 1</label>
+                                        <div class="admin_upload">
+
+                                            <label class="admin-upload-wrap">
+                                                <input type="file" class="form-control mt-3 uploadFile" name="image_2"
+                                                    id="image_2_about"
+                                                    accept="image/png, image/jpg, image/jpeg, image/webp">
+
+                                            </label>
+
+                                            <div class="profile_image">
+
+                                                @if ($items['about']['image_2'])
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ $items['about']['image_2'] }}" width="148px"
+                                                        height="221px">
+                                                @else
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ asset('asset/images/default_image.png') }}"
+                                                        width="148px" height="221px">
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="image">About Badge Text 2</label>
+                                        <input type="text" class="form-control text-input" name="about_badge_text1"
+                                            value="" placeholder="About Badge Text 2">
+
+                                        <label for="image">About Badge Icon 2</label>
+                                        <div class="admin_upload">
+
+                                            <label class="admin-upload-wrap">
+                                                <input type="file" class="form-control mt-3 uploadFile" name="image_2"
+                                                    id="image_2_about"
+                                                    accept="image/png, image/jpg, image/jpeg, image/webp">
+
+
+                                            </label>
+
+                                            <div class="profile_image">
+
+                                                @if ($items['about']['image_2'])
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ $items['about']['image_2'] }}" width="148px"
+                                                        height="221px">
+                                                @else
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ asset('asset/images/default_image.png') }}"
+                                                        width="148px" height="221px">
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="image">About Badge Text 3</label>
+                                        <input type="text" class="form-control text-input" name="about_badge_text1"
+                                            value="" placeholder="About Badge Text 1">
+
+                                        <label for="image">About Badge Icon 3</label>
+                                        <div class="admin_upload">
+
+                                            <label class="admin-upload-wrap">
+                                                <input type="file" class="form-control mt-3 uploadFile" name="image_2"
+                                                    id="image_2_about"
+                                                    accept="image/png, image/jpg, image/jpeg, image/webp">
+
+
+                                            </label>
+
+                                            <div class="profile_image">
+
+                                                @if ($items['about']['image_2'])
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ $items['about']['image_2'] }}" width="148px"
+                                                        height="221px">
+                                                @else
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ asset('asset/images/default_image.png') }}"
+                                                        width="148px" height="221px">
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                     <div class="form-group col-md-4">
+                                        <label for="image">About Badge Text 4</label>
+                                        <input type="text" class="form-control text-input" name="about_badge_text1"
+                                            value="" placeholder="About Badge Text 4">
+
+                                        <label for="image">About Badge Icon 4</label>
+                                        <div class="admin_upload">
+
+                                            <label class="admin-upload-wrap">
+                                                <input type="file" class="form-control mt-3 uploadFile" name="image_2"
+                                                    id="image_2_about"
+                                                    accept="image/png, image/jpg, image/jpeg, image/webp">
+
+
+                                            </label>
+
+                                            <div class="profile_image">
+
+                                                @if ($items['about']['image_2'])
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ $items['about']['image_2'] }}" width="148px"
+                                                        height="221px">
+                                                @else
+                                                    <img class="profile_img" id="thumbnail_show_image_2_about"
+                                                        src="{{ asset('asset/images/default_image.png') }}"
+                                                        width="148px" height="221px">
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
 
                             </div>
